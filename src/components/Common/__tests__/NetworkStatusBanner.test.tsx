ae220c014882eafc9f721dd8a070dc6593e0344d
_woo_s.j <sjwoo1999@korea.ac.kr>
2025-10-24 16:53:43 +0900

    🔨 Modified: NetworkStatusBanner.test.tsx
    Modified: NetworkStatusBanner.test.tsx

--- src/components/Common/__tests__/NetworkStatusBanner.test.tsx
+++ src/components/Common/__tests__/NetworkStatusBanner.test.tsx
@@ -1,11 +1,19 @@
 import { render, screen } from '@testing-library/react';
 import React from 'react';
 import { NetworkStatusBanner } from '../NetworkStatusBanner';
+import { SettingsProvider } from '../../../../contexts/SettingsContext';
+import { I18nProvider } from '../../../../contexts/I18nContext';
 
 describe('NetworkStatusBanner', () => {
   it('shows offline message when offline', () => {
     Object.defineProperty(window.navigator, 'onLine', { value: false, configurable: true });
-    render(<NetworkStatusBanner />);
+    render(
+      <SettingsProvider>
+        <I18nProvider>
+          <NetworkStatusBanner />
+        </I18nProvider>
+      </SettingsProvider>
+    );
     expect(screen.getByText(/오프라인 상태입니다/)).toBeInTheDocument();
   });
 });
